74b86255bbd9286d3c023c301233c551872ad64f
Francisco Araujo Ruiz <franxaraujo@gmail.com>
2022-02-16 07:14:48 -0300

    Incorporar MUI Icon Material

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Container from '@mui/material/Container';
 import ItemListContainer from './components/ItemListContainer';
+import SearchAppBar from './components/AppBar';
 
 function App() {
   return (
